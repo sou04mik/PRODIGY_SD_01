ca05482668b07eda3c16ad5b2146ba640862813a
sou04mik <166896023+sou04mik@users.noreply.github.com>
2024-05-05 02:00:46 +0530

    Add files via upload

---
+++ Temparature.py
@@ -0,0 +1,5 @@
+celcius=float(input("Enter any Temperature:"))
+fahrenheit=(9*celcius/5)+32
+print("The Equivalent temperature in fahrenheit is:",fahrenheit)
+kelvin=celcius+273.15
+print("The Equivalent temperature in kelvin is:",kelvin)
